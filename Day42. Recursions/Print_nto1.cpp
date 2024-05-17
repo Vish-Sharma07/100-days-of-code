2060a85c8ad2753c826f6db9d127d41bfc164f03
Vish-Sharma07 <120493508+Vish-Sharma07@users.noreply.github.com>
2024-05-17 10:04:12 +0530

    Create Print_nto1.cpp

---
+++ Day42. Recursions/Print_nto1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void print(int n){
+    if (n==0) return;
+    cout<<n<<endl;
+    print(n-1);
+}
+int main(){
+    int n;
+    cout<<"Enter n :";
+    cin>>n;
+    print(n);
+}
